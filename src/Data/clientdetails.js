da1034173d7eb2cf4012921bad49171f16351426
“Kunal <kunalkoushik44@gmail.com>
2024-03-13 11:28:50 +0530

    update

--- src/Data/clientdetails.js
+++ src/Data/clientdetails.js
@@ -1,34 +1,41 @@
-
-const NW = "https://res.cloudinary.com/di9itdore/image/upload/v1707212060/Shazofyne/Logos/NcW_logo_mrsb8w.jpg";
-const PW = "https://res.cloudinary.com/di9itdore/image/upload/v1707212060/Shazofyne/Logos/Physics_wallah_logo_1_ild6fn.jpg";
+const NW =
+  "https://res.cloudinary.com/di9itdore/image/upload/v1707212060/Shazofyne/Logos/NcW_logo_mrsb8w.jpg";
+const PW =
+  "https://res.cloudinary.com/di9itdore/image/upload/v1707212060/Shazofyne/Logos/Physics_wallah_logo_1_ild6fn.jpg";
 const HO =
   "https://res.cloudinary.com/di9itdore/image/upload/v1710062104/Shazofyne/Logos/channels4_profile_2_lx12em.jpg";
-const MD = "https://res.cloudinary.com/di9itdore/image/upload/v1707212063/Shazofyne/Logos/mia_ta1vrd.jpg";
-const STv = "https://res.cloudinary.com/di9itdore/image/upload/v1707212059/Shazofyne/Logos/Sorted_Tv_pic_l6qxx6.jpg";
+const MD =
+  "https://res.cloudinary.com/di9itdore/image/upload/v1707212063/Shazofyne/Logos/mia_ta1vrd.jpg";
+const STv =
+  "https://res.cloudinary.com/di9itdore/image/upload/v1707212059/Shazofyne/Logos/Sorted_Tv_pic_l6qxx6.jpg";
 const HS =
   "https://res.cloudinary.com/di9itdore/image/upload/v1710062104/Shazofyne/Logos/channels4_profile_3_bkvvxf.jpg";
 
-const GB = "https://res.cloudinary.com/di9itdore/image/upload/v1707212059/Shazofyne/Logos/gabe_wtwfqu.jpg";
-const FQ = "https://res.cloudinary.com/di9itdore/image/upload/v1707212059/Shazofyne/Logos/Faares_Q_fqch8o.jpg";
-const LS = "https://res.cloudinary.com/di9itdore/image/upload/v1707212066/Shazofyne/Logos/loststoner_od7a21.png";
-const MC = "https://res.cloudinary.com/di9itdore/image/upload/v1707212058/Shazofyne/Logos/themoneyceo_kqpqex.jpg";
+const GB =
+  "https://res.cloudinary.com/di9itdore/image/upload/v1707212059/Shazofyne/Logos/gabe_wtwfqu.jpg";
+const FQ =
+  "https://res.cloudinary.com/di9itdore/image/upload/v1707212059/Shazofyne/Logos/Faares_Q_fqch8o.jpg";
+const LS =
+  "https://res.cloudinary.com/di9itdore/image/upload/v1707212066/Shazofyne/Logos/loststoner_od7a21.png";
+const MC =
+  "https://res.cloudinary.com/di9itdore/image/upload/v1707212058/Shazofyne/Logos/themoneyceo_kqpqex.jpg";
 
 export const ClientDetails = [
-    {
-        title: `Physics Wallah`,
-        Sub: `11M+ Subscribers`,
-        image: PW,
-    },
-    {
-        title: `NCERT Wallah`,
-        Sub: `1.21M+ Subscribers`,
-        image: NW,
+  {
+    title: `Physics Wallah`,
+    Sub: `11M+ Subscribers`,
+    image: PW,
   },
-        {
-          title: `Homeys`,
-          Sub: `2M+ Subscribers`,
-          image: HO,
-        },
+  {
+    title: `NCERT Wallah`,
+    Sub: `1.21M+ Subscribers`,
+    image: NW,
+  },
+  {
+    title: `Homeys`,
+    Sub: `2M+ Subscribers`,
+    image: HO,
+  },
   {
     title: `Gabe Bult`,
     Sub: `500K+ Subscribers`,
--- src/components/common/Clients.js
+++ src/components/common/Clients.js
@@ -7,11 +7,11 @@ const Clients = () => {
       <h1 className=" md:w-auto font-bold mb-2 text-black  bg-yellow-50 rounded-md p-3 sm:text-3xl w-full justify-center items-center flex md:text-6xl  ">
         Clients
       </h1>
-      <div className="flex sm:flex-wrap md:items-center sm:justify-evenly md:justify-around  md:w-[100%] mt-2 sm:gap-4">
+      <div className="flex sm:flex-wrap md:items-center justify-evenly  md:w-[100%] mt-2 sm:gap-4">
 
       {ClientDetails.map((link, index)=>(
-        <div key={index} className="m-2  sm:w-[30%] sm:h-[30%] sm+:w-[20%] sm+:h[20%] md:h-[80%] md:w-[16%]  space-y-2 ">
-          <img src={link?.image} className=" h-[100%] bg-richblack-800  rounded-full " alt={link?.title}  loading="lazy"/>
+        <div key={index} className="flex flex-col m-2 items-center justify-center  sm:w-[30%] sm:h-[30%] sm+:w-[20%] sm+:h[20%] md:h-[80%] md:w-[16%]  space-y-2 ">
+          <img src={link?.image} className=" bg-richblack-800  rounded-full " alt={link?.title}  loading="lazy"/>
           <p className="text-center mt-1 text-yellow-50  font-bold md:text-2xl  ">{link?.title}</p>
           <p className="text-center mt-1 md:text-xl  ">{link?.Sub}</p>
         </div>
